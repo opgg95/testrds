688ae6629ba725d1cfda16908071152507162fea
opgg95 <ywc12345678@naver.com>
2023-08-10 17:12:45 +0900

    second commit

--- appRDS/src/test/java/org/zerock/apprds/AppRdsApplicationTests.java
+++ appRDS/src/test/java/org/zerock/apprds/AppRdsApplicationTests.java
@@ -7,7 +7,7 @@ import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import lombok.RequiredArgsConstructor;
+
 
 @SpringBootTest
 class AppRdsApplicationTests {
